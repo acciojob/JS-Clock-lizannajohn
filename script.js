9939c6c7d9d2f4b551bd58712eb21578f51f982f
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-31 20:35:54 +0400

    js clock solved

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function setDate()
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    secondsHand.style.transform = `rotate(${secondsDegrees + 90}deg)`;
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    minsHand.style.transform = `rotate(${minsDegrees + 90}deg)`;
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees + 90}deg)`;
 }
 
 setInterval(setDate, 1000)
